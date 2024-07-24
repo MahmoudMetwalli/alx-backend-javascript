ca2dd61d0a0f3f5d441d245aa0d31e1434b2b677
MahmoudMetwalli <mammkbih@gmail.com>
2024-07-25 02:34:08 +0300

    2024-07-25 02:34:08 AM

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 export default function getResponseFromAPI() {
-	const promise = new Promise(function(resolve, reject) {
-		  resolve("Stuff worked!");
-	  });
-	return promise;
+  const promise = new Promise((resolve) => {
+    resolve('Stuff worked!');
+  });
+  return promise;
 }
