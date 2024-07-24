3d1ed6bd95be84148c9b28a8ac9c7e3329eaf6f8
MahmoudMetwalli <mammkbih@gmail.com>
2024-07-24 23:07:43 +0300

    2024-07-24 11:07:43 PM

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise(function(resolve, reject) {
+    if (success) {
+      resolve({status: 200,
+		body: 'Success'});
+    }
+    else {
+      reject(Error("The fake API is not working currently"));
+    }
+	});
+  return promise;
+}
