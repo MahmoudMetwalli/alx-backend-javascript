8789608479170b87af14b5479bb670e3eb4b7a4e
MahmoudMetwalli <mammkbih@gmail.com>
2024-07-25 04:27:03 +0300

    2024-07-25 04:27:03 AM

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]).then((value) => console.log(value));
+  return Promise.any([chinaDownload, USDownload]).then((value) => value);
 }
